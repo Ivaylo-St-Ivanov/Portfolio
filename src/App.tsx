38ceeb80e27947fd35b1c9dcc2e07735b0cf5e9f
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-02-09 20:02:39 +0200

    Main page created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 
-// import './App.css';
+import './App.scss';
 
 function App() {
 
     return (
-        <>
-            <h1>Vite + React + TS</h1>
-        </>
+        <main className="wrapper">
+            <div>
+                <img src="/images/image5.jpeg" alt="Ivaylo's photo" />
+            </div>
+            <div className="wrapper__overlay"></div>
+
+            <div className="wrapper__title">
+                <div>
+                    <span>&lt;/ Hi, There &gt;</span>
+                    <h1>I'm Ivo and I'm a</h1>
+                    <h2>JS Web Developer</h2>
+                </div>
+            </div>
+        </main>
     );
 }
 
