4f2d93a203283fa4e94b5b91466a3de5c232d706
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-02-10 08:37:51 +0200

    Install 'react icons' and add header and footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
+import { FaRegCopyright } from 'react-icons/fa6';
 
 import './App.scss';
 
 function App() {
 
     return (
-        <main className="wrapper">
-            <div>
-                <img src="/images/image5.jpeg" alt="Ivaylo's photo" />
-            </div>
-            <div className="wrapper__overlay"></div>
+        <>
+            <header>
+                <input type="button" value="About Me" />
+                <input type="button" value="Portfolio" />
+            </header>
 
-            <div className="wrapper__title">
+            <main className="wrapper">
                 <div>
-                    <span>&lt;/ Hi, There &gt;</span>
-                    <h1>I'm Ivo and I'm a</h1>
-                    <h2>JS Web Developer</h2>
+                    <img src="/images/image5.jpeg" alt="Ivaylo's photo" />
                 </div>
-            </div>
-        </main>
+                <div className="wrapper__overlay"></div>
+
+                <div className="wrapper__title">
+                    <div>
+                        <span>&lt;/ Hi, There &gt;</span>
+                        <h1>I'm Ivo and I'm a</h1>
+                        <h2>JS Web Developer</h2>
+                    </div>
+                </div>
+            </main>
+
+            <footer>
+                <span><FaRegCopyright /> Ivaylo Ivanov</span>
+            </footer>
+        </>
     );
 }
 
